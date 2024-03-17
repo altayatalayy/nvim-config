94a393a4117f7d4cee0c56257f13562cb3f91fbd
Altay Atalay <altayatalay1998@gmail.com>
2024-03-17 21:01:52 +0300

    update: detect zsh config functions

--- init.lua
+++ init.lua
@@ -5,16 +5,29 @@ require("user.autocmds")
 -- clone lazy and add it to the rtp
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- setup plugins
 require("lazy").setup("user.plugins")
+
+-- autocommand for detecting zsh functions
+local zdotdir = os.getenv("ZDOTDIR") or ""
+if zdotdir ~= "" then
+    local functions_dir = zdotdir .. "/functions/*"
+
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = functions_dir,
+        callback = function()
+            vim.bo.filetype = "zsh"
+        end,
+    })
+end
--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,65 +1,65 @@
 return {
-	-- Telescope
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
-	{
-		"AckslD/nvim-neoclip.lua",
-		dependencies = {
-			{ "kkharji/sqlite.lua", module = "sqlite" },
-		},
-		config = function()
-			local status_ok, neoclip = pcall(require, "neoclip")
-			if not status_ok then
-				return
-			end
-			neoclip.setup()
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		keys = {
-			{
-				"<leader>tf",
-				function()
-					vim.cmd([[ Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍 ]])
-				end,
-				desc = "find files",
-			},
-			{ "<leader>tr", ":Telescope file_browser<CR>", desc = "file browser" },
-			{ "<leader>tl", ":Telescope live_grep<CR>", desc = "live grep" },
-			{ "<leader>tk", ":Telescope keymaps<CR>", desc = "keymaps" },
-			{ "<leader>td", ":Telescope diagnostics<CR>", desc = "diagnostics" },
-			{ "<leader>tc", ":Telescope neoclip<CR>", desc = "neoclip" },
-			{
-				"<leader>tg",
-				function()
-					require("telescope.builtin").lsp_references()
-				end,
-				desc = "find references",
-			},
-			{
-				"<leader>tx",
-				":lua require('telescope.builtin').lsp_workspace_symbols({query=vim.call('expand','<cword>')})<CR>",
-				desc = "find symbol",
-			},
-			{
-				"<leader>ti",
-				":lua require('telescope.builtin').lsp_implementations<CR>",
-				desc = "implementations",
-			},
-			{
-				"<leader>td",
-				":lua require('telescope.builtin').lsp_definitions<CR>",
-				desc = "definitions",
-			},
-			{
-				"<leader>th",
-				":Telescope harpoon marks<CR>",
-				desc = "harpoon marks",
-			},
+    -- Telescope
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
+    {
+        "AckslD/nvim-neoclip.lua",
+        dependencies = {
+            { "kkharji/sqlite.lua", module = "sqlite" },
+        },
+        config = function()
+            local status_ok, neoclip = pcall(require, "neoclip")
+            if not status_ok then
+                return
+            end
+            neoclip.setup()
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        keys = {
+            {
+                "<leader>tf",
+                function()
+                    vim.cmd([[ Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍 ]])
+                end,
+                desc = "find files",
+            },
+            { "<leader>tr", ":Telescope file_browser<CR>", desc = "file browser" },
+            { "<leader>tl", ":Telescope live_grep<CR>",    desc = "live grep" },
+            { "<leader>tk", ":Telescope keymaps<CR>",      desc = "keymaps" },
+            { "<leader>td", ":Telescope diagnostics<CR>",  desc = "diagnostics" },
+            { "<leader>tc", ":Telescope neoclip<CR>",      desc = "neoclip" },
+            {
+                "<leader>tg",
+                function()
+                    require("telescope.builtin").lsp_references()
+                end,
+                desc = "find references",
+            },
+            {
+                "<leader>tx",
+                ":lua require('telescope.builtin').lsp_workspace_symbols({query=vim.call('expand','<cword>')})<CR>",
+                desc = "find symbol",
+            },
+            {
+                "<leader>ti",
+                ":lua require('telescope.builtin').lsp_implementations<CR>",
+                desc = "implementations",
+            },
+            {
+                "<leader>td",
+                ":lua require('telescope.builtin').lsp_definitions<CR>",
+                desc = "definitions",
+            },
+            {
+                "<leader>th",
+                ":Telescope harpoon marks<CR>",
+                desc = "harpoon marks",
+            },
             {
                 "<leader>tw",
                 "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
@@ -70,76 +70,76 @@ return {
                 "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
                 desc = "create_git_worktree",
             },
-		},
-		cmd = "Telescope",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"rcarriga/nvim-notify",
-			"nvim-telescope/telescope-media-files.nvim",
-			"nvim-telescope/telescope-dap.nvim",
-			"camgraff/telescope-tmux.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-			"nvim-telescope/telescope-file-browser.nvim",
-			"nvim-telescope/telescope-fzf-native.nvim",
-			"ThePrimeagen/harpoon",
+        },
+        cmd = "Telescope",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "rcarriga/nvim-notify",
+            "nvim-telescope/telescope-media-files.nvim",
+            "nvim-telescope/telescope-dap.nvim",
+            "camgraff/telescope-tmux.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
+            "nvim-telescope/telescope-fzf-native.nvim",
+            "ThePrimeagen/harpoon",
             'ThePrimeagen/git-worktree.nvim',
-		},
-		config = function()
-			local status_ok, telescope = pcall(require, "telescope")
-			if not status_ok then
-				return
-			end
+        },
+        config = function()
+            local status_ok, telescope = pcall(require, "telescope")
+            if not status_ok then
+                return
+            end
 
-			local status_ok, actions = pcall(require, "telescope.actions")
-			if not status_ok then
-				return
-			end
+            local status_ok, actions = pcall(require, "telescope.actions")
+            if not status_ok then
+                return
+            end
 
-			telescope.setup({
-				defaults = {
-					file_ignore_patterns = { ".git/" },
-					mappings = {
-						i = {
-							["<C-j>"] = {
-								actions.move_selection_next,
-								type = "action",
-								opts = { nowait = true, silent = true },
-							},
-							["<C-k>"] = {
-								actions.move_selection_previous,
-								type = "action",
-								opts = { nowait = true, silent = true },
-							},
-							["<C-y>"] = {
-								actions.select_default,
-								type = "action",
-								opts = { nowait = true, silent = true },
-							},
-						},
-					},
-				},
-			})
+            telescope.setup({
+                defaults = {
+                    file_ignore_patterns = { ".git/" },
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = {
+                                actions.move_selection_next,
+                                type = "action",
+                                opts = { nowait = true, silent = true },
+                            },
+                            ["<C-k>"] = {
+                                actions.move_selection_previous,
+                                type = "action",
+                                opts = { nowait = true, silent = true },
+                            },
+                            ["<C-y>"] = {
+                                actions.select_default,
+                                type = "action",
+                                opts = { nowait = true, silent = true },
+                            },
+                        },
+                    },
+                },
+            })
 
-			telescope.load_extension("file_browser")
-			telescope.load_extension("ui-select")
-			telescope.load_extension("fzf")
-			telescope.load_extension("neoclip")
-			telescope.load_extension("media_files")
-			telescope.load_extension("dap")
-			telescope.load_extension("notify")
-			telescope.load_extension("tmux")
-			telescope.load_extension("harpoon")
+            telescope.load_extension("file_browser")
+            telescope.load_extension("ui-select")
+            telescope.load_extension("fzf")
+            telescope.load_extension("neoclip")
+            telescope.load_extension("media_files")
+            telescope.load_extension("dap")
+            telescope.load_extension("notify")
+            telescope.load_extension("tmux")
+            telescope.load_extension("harpoon")
             telescope.load_extension("git_worktree")
-		end,
-		init = function()
-			--vim.keymap.set('n', '<leader>tf', function () vim.cmd[[ Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<CR> ]] end, {desc = 'find files'})
-			--vim.keymap.set('n', '<leader>tg', ':Telescope git_files<CR>', {desc = 'git files'})
-			--vim.keymap.set('n', '<leader>tl', ':Telescope live_grep<CR>', {desc = 'live grep'})
-			--vim.keymap.set('n', '<leader>tk', ':Telescope keymaps<CR>', {desc = 'keymaps'})
-			--vim.keymap.set('n', '<leader>tb', ':Telescope buffers<CR>', {desc = 'buffers'})
-			--vim.keymap.set('n', '<leader>tc', ':Telescope neoclip<CR>', {desc = 'neoclip'})
-			--vim.keymap.set('n', '<leader>td', ':Telescope diagnostics<CR>', {desc = 'diagnostics'})
-			--vim.keymap.set('n', '<leader>tm', ':lua require("telescope").extensions.macroscope.default()<CR>', {desc = 'macroscope'})
-		end,
-	},
+        end,
+        init = function()
+            --vim.keymap.set('n', '<leader>tf', function () vim.cmd[[ Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<CR> ]] end, {desc = 'find files'})
+            --vim.keymap.set('n', '<leader>tg', ':Telescope git_files<CR>', {desc = 'git files'})
+            --vim.keymap.set('n', '<leader>tl', ':Telescope live_grep<CR>', {desc = 'live grep'})
+            --vim.keymap.set('n', '<leader>tk', ':Telescope keymaps<CR>', {desc = 'keymaps'})
+            --vim.keymap.set('n', '<leader>tb', ':Telescope buffers<CR>', {desc = 'buffers'})
+            --vim.keymap.set('n', '<leader>tc', ':Telescope neoclip<CR>', {desc = 'neoclip'})
+            --vim.keymap.set('n', '<leader>td', ':Telescope diagnostics<CR>', {desc = 'diagnostics'})
+            --vim.keymap.set('n', '<leader>tm', ':lua require("telescope").extensions.macroscope.default()<CR>', {desc = 'macroscope'})
+        end,
+    },
 }
